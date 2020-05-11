64c8b459e2d7cf56f13285ba8254289ab27f673f
Thinh Le <thinh.l@techchain.vn>
2020-05-11 17:22:45 +0700

    Draw graph line

---
+++ Test/Core Graphics/Flo/CoreGraphicsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  CoreGraphicsViewController.swift
+//  Test
+//
+//  Created by Techchain on 5/11/20.
+//  Copyright © 2020 Techchain. All rights reserved.
+//
+
+import UIKit
+
+class CoreGraphicsViewController: UIViewController {
+    @IBOutlet weak var counterView: CounterView!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
+    
+    var isGraphViewShowing = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        counterLabel.text = String(counterView.counter)
+    }
+    
+    @IBAction func pushButtonPressed(_ button: PushButton) {
+        if button.isAddButton {
+            counterView.counter += 1
+        } else {
+            if counterView.counter > 0 {
+                counterView.counter -= 1
+            }
+        }
+        counterLabel.text = String(counterView.counter)
+        if isGraphViewShowing {
+            counterViewTap(nil)
+        }
+    }
+    
+    @IBAction func counterViewTap(_ gesture: UITapGestureRecognizer?) {
+        if (isGraphViewShowing) {
+            UIView.transition(from: graphView,
+                              to: counterView,
+                              duration: 1.0,
+                              options: [.transitionFlipFromLeft, .showHideTransitionViews],
+                              completion:nil)
+        } else {
+            UIView.transition(from: counterView,
+                              to: graphView,
+                              duration: 1.0,
+                              options: [.transitionFlipFromRight, .showHideTransitionViews],
+                              completion: nil)
+        }
+        isGraphViewShowing = !isGraphViewShowing
+    }
+}
